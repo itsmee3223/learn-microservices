92c2088c5cff9218f6c5b1d89e8a76daa445db74
itsmee3223 <tamvam.pemberani@gmail.com>
2022-11-14 21:33:58 +0700

    integrate routes

--- ticketing/auth/src/index.ts
+++ ticketing/auth/src/index.ts
@@ -1,5 +1,13 @@
 import express from "express";
 
+import { NotFoundError } from "./errors/notFoundError";
+import { errorHandler } from "./middlewares/errorHandler";
+
+import { currentUFserRouter } from "./routes/currentUser.routes";
+import { signinRouter } from "./routes/signin.routes";
+import { signoutRouter } from "./routes/signout.routes";
+import { signupRouer } from "./routes/signup.routes";
+
 const app = express();
 app.use(express.json());
 
@@ -8,6 +16,17 @@ app.get("/", (req, res) => {
     message: "Server is running on port 3000",
   });
 });
+
+app.use("/api/v1/users", signinRouter);
+app.use("/api/v1/users", signupRouer);
+app.use("/api/v1/users", signoutRouter);
+app.use("/api/v1/users", currentUFserRouter);
+
+app.all("*", () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
