9551d4d2ac6f7e9ed8d423de6bcca714ab634682
itsmee3223 <tamvam.pemberani@gmail.com>
2022-11-16 11:41:46 +0700

    signup routes

--- ticketing/auth/src/routes/signup.routes.ts
+++ ticketing/auth/src/routes/signup.routes.ts
@@ -1,7 +1,9 @@
 import { Router, Request, Response } from "express";
 import { body, validationResult } from "express-validator";
+import { BadRequestError } from "../errors/badRequestError";
 import { DatabaseConnectionError } from "../errors/databaseConnectionError";
 import { RequestValidationError } from "../errors/requestValidationError";
+import { User } from "../models/User.model";
 
 const router = Router();
 
@@ -21,10 +23,17 @@ router.post(
       throw new RequestValidationError(errors.array());
     }
 
-    console.log("Creating a user...");
-    throw new DatabaseConnectionError();
+    const { email, password } = req.body;
 
-    return res.send();
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      throw new BadRequestError("Email in user");
+    }
+
+    const user = User.build({ email, password });
+    await user.save();
+
+    return res.status(201).json(user);
   }
 );
 
