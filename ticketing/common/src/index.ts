a5d2871622e8d48146fff138870e3129d2dc73a5
itsmee3223 <tamvam.pemberani@gmail.com>
2022-12-18 09:13:58 +0700

    order event created & cancelled

--- ticketing/common/src/index.ts
+++ ticketing/common/src/index.ts
@@ -11,8 +11,12 @@ export * from './middlewares/error-handler';
 export * from './middlewares/require-auth';
 export * from './middlewares/validate-request';
 
-export * from "./events/base-listener";
-export * from "./events/base-publisher";
-export * from "./events/subjects";
-export * from "./events/ticket-created-event";
-export * from "./events/ticket-updated-events";
+// export event
+export * from './events/base-listener';
+export * from './events/base-publisher';
+export * from './events/subjects';
+export * from './events/ticket-created-event';
+export * from './events/ticket-updated-event';
+export * from './events/types/order-status';
+export * from './events/order-cancelled-event';
+export * from './events/order-created-event';
