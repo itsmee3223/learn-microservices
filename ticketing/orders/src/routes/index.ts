9c2f4036df55fa904ea52ac2270c952789d5be53
itsmee3223 <tamvam.pemberani@gmail.com>
2022-12-21 21:25:16 +0700

    add version prop to ticket

--- ticketing/orders/src/routes/index.ts
+++ ticketing/orders/src/routes/index.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from 'express';
-import { requireAuth } from '@cygnetops/common';
+import { requireAuth } from '@itsme33/common';
 import { Order } from '../models/order';
 
 const router = express.Router();
--- ticketing/orders/src/routes/show.ts
+++ ticketing/orders/src/routes/show.ts
@@ -3,7 +3,7 @@ import {
   requireAuth,
   NotFoundError,
   NotAuthorizedError,
-} from '@cygnetops/common';
+} from '@itsme33/common';
 import { Order } from '../models/order';
 
 const router = express.Router();
