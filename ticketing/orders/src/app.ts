f532682e0a4c6165a8644a89f5fa96c4bc0a3e04
itsmee3223 <tamvam.pemberani@gmail.com>
2022-12-21 21:25:45 +0700

    add all route to app

--- ticketing/orders/src/app.ts
+++ ticketing/orders/src/app.ts
@@ -1,21 +1,31 @@
-import express from "express";
-import "express-async-errors";
-import cookieSeesion from "cookie-session";
-import { errorHandler, NotFoundError, currentUser } from "@itsme33/common";
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError, currentUser } from '@itsme33/common';
+
+import { deleteOrderRouter } from './routes/delete';
+import { indexOrderRouter } from './routes/index';
+import { newOrderRouter } from './routes/new';
+import { showOrderRouter } from './routes/show';
 
 const app = express();
-app.set("trust proxy", true);
-app.use(express.json());
+app.set('trust proxy', true);
+app.use(json());
 app.use(
-  cookieSeesion({
+  cookieSession({
     signed: false,
-    secure: process.env.NODE_ENV === "test",
+    secure: process.env.NODE_ENV !== 'test',
   })
 );
-
 app.use(currentUser);
 
-app.all("*", async (req, res) => {
+app.use(deleteOrderRouter);
+app.use(indexOrderRouter);
+app.use(newOrderRouter);
+app.use(showOrderRouter);
+
+app.all('*', async (req, res) => {
   throw new NotFoundError();
 });
 
